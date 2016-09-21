9502e8172ee58a5f828c97d624064b41fde8c965
airjqqq <airjqqq@qq.com>
2016-09-21 12:19:23 +0800

    guild

--- AirjGuild/AirjGuild.lua
+++ AirjGuild/AirjGuild.lua
@@ -15,6 +15,36 @@ function Core:OnDisable()
 
 end
 
+local guildRosterInfo={}
+function Core:UpdateGuildRosterInfo()
+  for i=1,GetNumGuildMembers() do
+    local fullName, rank, rankIndex, level, class, zone, note, officernote, online, status, classFileName, achievementPoints, achievementRank, isMobile, canSoR, reputation = GetGuildRosterInfo(i)
+    local data = guildRosterInfo[fullName] or {}
+    if online then data.lastOnline = GetTime() end
+    data.note = officernote
+    guildRosterInfo[fullName] = data
+  end
+end
+
+function Core:IsUnitPresent(unit)
+  if UnitInRaid(name) or standbyUnit[name] then
+    return true
+  end
+  return false
+end
+
+function Core:UpdateParticipation()
+  local hour, minute = GetGameTime()
+  hour = hour+minute/30
+  local weekday, month, day, year = CalendarGetDate()
+  local inRaid = IsInRaid("player")
+  if weekday>=2 and weekday<=5 and hour>=8.5 and hour<=11.5 and inRaid then
+    for name, data in pairs(guildRosterInfo) do
+      if GetTime() - data.lastOnline <600 then
+      end
+    end
+  end
+end
 
 -- LOOT iLVL Save
 
